991427a98f383a4ba3b573324d3605f1572677d9
lanius <mohammedno1@proton.me>
2026-01-16 23:24:07 +0200

    Initial commit - Ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      // أضف السطر التالي هنا
-      base: '/Why-should-I-quit-my-job-and-become-a-goat-farmer/', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // المسار الأساسي للمستودع على GitHub
+  base: '/Why-should-I-quit-my-job-and-become-a-goat-farmer/',
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      // إعداد الاختصارات للمجلدات
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
